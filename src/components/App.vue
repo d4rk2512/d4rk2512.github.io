4a82f06f1d8f63cd48c6a868d5f3be56ef201fe3
Lê Hải Nam <L Hi Nam>
2017-03-19 23:26:22 +0700

    20170319.2

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,20 +1,35 @@
 <template>
 	<div id="app">
-		<top-header></top-header>
+		<top-header :user='user'></top-header>
 	</div>
 </template>
 
 <script>
 	import Vue from 'vue'
-	import { FirebaseDB } from './../Libs/FirebaseManager.js'
+	import { FirebaseAuth, FirebaseDB } from './../Libs/FirebaseManager.js'
 	import TopHeader from './TopHeader.vue'
 
 	export default {
 		name: 'app',
+		data() {
+			return {
+				user: FirebaseAuth.currentUser
+			}
+		},
+		created: function () {
+			FirebaseAuth.onAuthStateChanged((user) => {
+				this.user = user
+			});
+		}
 	}
 
 </script>
 
 <style>
-
+	html,
+	body {
+		font-family: Roboto, Arial, sans-serif;
+		background-color: #ECEFF1;
+		overflow: hidden;
+	}
 </style>
--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,18 +1,69 @@
 <template>
-	<div>
-		<h3>Hardcorers</h3>
-	</div>
+	<nav>
+		<div class='left-content'>
+			<div class='brand'>
+				<h3>Hardcorers</h3>
+			</div>
+		</div>
+
+		<div class='right-content'>
+			<div v-if='user == null'>
+				<button @click='login'>Đăng nhập</button>
+
+			</div>
+			<div v-else>Welcome, {{ user.userName }}</div>
+		</div>
+	</nav>
 </template>
 
 <script>
 	import Vue from 'vue'
 
 	export default Vue.component('top-header', {
+		props: ['user'],
+		methods: {
+			login() {
+				console.log('login clicked')
+			}
+		}
 	})
 
 </script>
 
 <style scoped>
+	nav {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		position: fixed;
+		height: 50px;
+		margin: 0;
+		padding: 0 20px;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		background-color: #fff;
+		box-shadow: 0 0 4px rgba(0, 0, 0, .14), 0 4px 8px rgba(0, 0, 0, .28);
+		z-index: 1000;
+	}
+	
+	.left-content {
 
-
+	}
+	
+	.brand {
+		display: flex;
+		vertical-align: middle;
+		cursor: pointer;
+	}
+	
+	.brand h3 {
+		margin: 14px;
+		margin-left: 0;
+	}
+	
+	.header-container {
+		float: right;
+	}
 </style>
